60ef8dcc7f886b1f822481b089c147a5a7ee34fb
unfair01 <Unfairerlowa@gmail.com>
2025-01-11 00:35:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const numImages = 14; // 총 이미지 개수 (4.jpg 제외)
+const numImages = 14;
 const images = [];
 for (let i = 1; i <= 15; i++) {
    if(i != 4) {
         images.push(`${i}.jpg`);
    }
 }
+
 
 let round = 1;
 let selected = [];
@@ -31,15 +32,19 @@ function showPair() {
         rightIndex = Math.floor(Math.random() * images.length);
     } while (leftIndex === rightIndex);
 
-    document.getElementById("leftImage").src = images[leftIndex];
-    document.getElementById("rightImage").src = images[rightIndex];
+     const leftImageElement = document.getElementById("leftImage");
+    const rightImageElement = document.getElementById("rightImage");
 
-    document.getElementById("leftButton").onclick = () => selectImage(leftIndex, rightIndex);
-    document.getElementById("rightButton").onclick = () => selectImage(rightIndex, leftIndex);
+    leftImageElement.src = images[leftIndex];
+    rightImageElement.src = images[rightIndex];
+
+
+    leftImageElement.onclick = () => selectImage(leftIndex, rightIndex);
+    rightImageElement.onclick = () => selectImage(rightIndex, leftIndex);
 }
 
 function selectImage(selectedIndex, otherIndex) {
-    selected.push(images[selectedIndex]);
+     selected.push(images[selectedIndex]);
     if (selectedIndex > otherIndex) {
          images.splice(otherIndex, 1);
          images.splice(selectedIndex - 1, 1);
@@ -50,17 +55,9 @@ function selectImage(selectedIndex, otherIndex) {
     showPair();
 }
 
-
 function showResult() {
     document.getElementById("container").style.display = "none";
-    document.getElementById("leftButton").style.display = "none";
-    document.getElementById("rightButton").style.display = "none";
-
-    if (selected.length === 1) {
-        document.getElementById("result").innerText = `최종 우승: ${selected[0]}`;
-    } else {
-        document.getElementById("result").innerText = "선택된 이미지가 없습니다.";
-    }
+    document.getElementById("result").innerText = `최종 우승: ${selected[0]}`;
 }
 
 window.onload = function() {
